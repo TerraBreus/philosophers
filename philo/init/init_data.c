7c7e518e4cb5822b33f7309a0e87b71b734902b8
TerraBreus <studentbreus@gmail.com>
2025-08-22 17:48:34 +0200

    adding initialization frame of main data

---
+++ philo/init/init_data.c
@@ -0,0 +1,11 @@
+int	init_data(int argc, char **argv)
+{
+	if (syntax_check(argc, argv) == false)
+		ft_usage();
+	// Initialize (if valid) the data structure.
+	data = init_data(argc, argv);
+	if (data == NULL)
+		exit(EXIT_FAILURE);
+	if (data_check(argc, data) == false)
+		ft_usage();
+}
