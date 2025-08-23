363f4d5cca1866b721ae259f5e48b4fe769094f8
TerraBreus <studentbreus@gmail.com>
2025-08-23 10:34:56 +0200

    refactor: organizer header into different sections

--- philo/philo.h
+++ philo/philo.h
@@ -1,3 +1,4 @@
+//xXx I N C L U D E S
 #include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -5,8 +6,10 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+//xXx M A C R O S
 #define USAGE "Usage: number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]*\n\n* If no parameter is given, program will run till infinitely (unless a philosopher dies)\n\nMax amount of philosophers is 200.\n"
 
+//xXx S T R U C T U R E S
 typedef enum e_action
 {
 	THINKING,
@@ -39,31 +42,7 @@ typedef	struct s_philo
 	action		state;
 }	t_philo;
 
-//FUNCTIONS
-
-//	for whenever a philosopher switches state, a message must be printed
-//	with the time, the philosopher in question, and the changed state (action).
-int	print_log(int timestamp, int philo_n, action action);
-
-//	In case of errors, calls clean_and_exit if data != NULL.
-void	ft_error(char *place_of_error, t_data *data);
-
-
-int	init_philo(t_data *data);
-int	init_forks(t_data *data);
-void	close_table(t_philo *philo);
-int	add_philo_to_table(t_philo *top_philo, int id);
-bool	data_check(int argc, t_data *data);
-t_data	*init_data(int argc, char **argv);
-int	ft_atoi(const char *nptr);
-static int	ft_calc_int(const char *str, size_t length, int conv_int);
-static size_t	ft_get_length(const char *input);
-int	ft_usage(void);
-bool	syntax_check(int argc, char **argv);
-void	*philo_routine(void *philo);
-void	ft_error(char *place_of_error, t_data *data);
-void	clean_and_exit(t_data *data);
-
+//xXx F U N C T I O N S -
 
 // 			-- D E B U G G I N G --
 // 			functions for debugging while program is in development.
@@ -76,15 +55,40 @@ void	print_philo(t_data *data);
 // 			mallocs, syntax checks,
 // 			creation of circular loops etc.
 
+int	init_philo(t_data *data);
+int	init_forks(t_data *data);
+void	close_table(t_philo *philo);
+int	add_philo_to_table(t_philo *top_philo, int id);
+bool	data_check(int argc, t_data *data);
+t_data	*init_data(int argc, char **argv);
+bool	syntax_check(int argc, char **argv);
+
 // 			-- U S E R -- U T I L S --
 // 			functions that give the user information about the program.
 
 void	ft_error(char *place_of_error, t_data *data);
+//	In case of errors, calls clean_and_exit if data != NULL.
 
-// -- P R O G R A M M E R -- U T I L S --
-// util functions that make the life of a programmer liveable.
+int	ft_usage(void);
+//	prints message to the user on how to
+//	(correctly) start the program.
 
-//	Returns the current time since the epoch in ms using 'gettimeofday'.
+int	print_log(int timestamp, int philo_n, action action);
+//	for whenever a philosopher switches state, a message must be printed
+//	with the time, the philosopher in question, and the changed state (action).
+
+
+// 			-- P R O G R A M M E R -- U T I L S --
+//			util functions that make the life of a programmer liveable.
+
 long	get_time();
+//	Returns the current time since the epoch in ms using 'gettimeofday'.
 
-// PHILOSOPHER LIFESTYLE
+int	ft_atoi(const char *nptr);
+//	Converts a string of numbers into an actual integer.
+
+void	clean_and_exit(t_data *data);
+
+// 			-- P H I L O S O P H E R -- L I F E S T Y L E --
+
+void	*philo_routine(void *philo);
