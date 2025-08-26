ad9c84a1f6edfa58def7e278629a59bce8f5e17e
TerraMint <studentbreus@gmail.com>
2025-08-26 10:59:17 +0200

    feat: alpha of ft_usage

--- philo/user_utils/ft_usage.c
+++ philo/user_utils/ft_usage.c
@@ -1,3 +1,20 @@
 #include "philo.h"
 
+int	ft_get_len(char *str)	//TODO put in own file.
+{
+	int	i;
 
+	i = 0;
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+int	ft_usage(void)
+{
+	write(STDERR_FILENO, USAGE, ft_get_len(USAGE));
+	return (-1);
+}
