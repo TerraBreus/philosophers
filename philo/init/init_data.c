14bc35dc3244719739b5f21441a94bdb5b53e55f
TerraMint <studentbreus@gmail.com>
2025-08-26 11:23:20 +0200

    fix: init for general data

--- philo/init/init_data.c
+++ philo/init/init_data.c
@@ -4,18 +4,17 @@ t_data	*init_data(int argc, char **argv)
 {
 	t_data	*result;
 
-	if (syntax_check(argc, argv) == false)
-	{
-		ft_usage();
-		return (NULL);
-	}
-	result = init_data(argc, argv);
+	result = (t_data *)malloc(sizeof(t_data));
 	if (result == NULL)
-		return (NULL);	
-	if (data_check(argc, result) == false)
-	{
-		ft_usage();
 		return (NULL);
-	}
+	result->start_time = get_time();
+	result->n_philo = ft_atoi(argv[1]);
+	result->time_to_die = ft_atoi(argv[2]);
+	result->time_to_eat = ft_atoi(argv[3]);
+	result->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		result->total_eat_limit = ft_atoi(argv[5]);
+	else 
+		result->total_eat_limit = -1;
 	return (result);
 }
--- philo/main.c
+++ philo/main.c
@@ -4,10 +4,21 @@ t_data	*init_all(int argc, char **argv)
 {
 	t_data	*data;
 
+	if (syntax_check(argc, argv) == false)
+	{
+		ft_usage();
+		return (NULL);
+	}
 	data = init_data(argc, argv);
 	if (data == NULL)
 		return (NULL);
-	init_philo(data);	//TODO: check for philo malloc (how?)
+	if (data_check(argc, data) == false)
+	{
+		ft_usage();
+		return (NULL);
+	}
+	if (init_philo(data) != 0)	//TODO: check for philo malloc (how?)
+		return (NULL);
 	return (data);
 }
 
