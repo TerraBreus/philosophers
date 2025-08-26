1c65c76c73a49a0640dd5c6bf12ad97c209321b2
TerraMint <studentbreus@gmail.com>
2025-08-26 10:59:02 +0200

    feat: added alpha of ft_error

--- philo/user_utils/ft_error.c
+++ philo/user_utils/ft_error.c
@@ -1,3 +1,9 @@
 #include "philo.h"
 
+void	ft_error(char *place_of_error, t_data *data)
+{
+	write(STDERR_FILENO, place_of_error, ft_get_len(place_of_error));
+	//TODO: clean and exit
+	data = NULL;
+}
 
