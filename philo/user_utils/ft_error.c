438c75eeb811f8f09345bb54cfd87f50f090da46
TerraMint <studentbreus@gmail.com>
2025-08-26 11:01:34 +0200

    fix: comp error in ft_error

--- philo/user_utils/ft_error.c
+++ philo/user_utils/ft_error.c
@@ -4,6 +4,7 @@ void	ft_error(char *place_of_error, t_data *data)
 {
 	write(STDERR_FILENO, place_of_error, ft_get_len(place_of_error));
 	//TODO: clean and exit
-	data = NULL;
+	if (data != NULL)
+		data = NULL;
 }
 
