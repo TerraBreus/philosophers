370dbe920dba5eea971c1c3bb3792900fc7a7ccb
TerraMint <studentbreus@gmail.com>
2025-09-04 17:16:25 +0200

    feat: created print_log

--- philo/user_utils/print_log.c
+++ philo/user_utils/print_log.c
@@ -1,3 +1,11 @@
 #include "philo.h"
 
+void	print_log(long timestamp,t_philo *philo, char *action)
+{
+	long	time;
 
+	time = timestamp - philo->data->start_time;
+	printf("%lu %i %s", time, philo->ID, action);
+}
+
+//I don't know what else to put here
