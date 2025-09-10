0b10172c500cee48c3107d8a58058ae6afe1e978
TerraMint <studentbreus@gmail.com>
2025-09-10 13:41:32 +0200

    feat: added routine for monitor/supervisor

---
+++ philo/routine/monitor_routine.c
@@ -0,0 +1,28 @@
+#include "philo.h"
+
+void	*monitor_routine(void *ptr)
+{
+	t_data	*data;
+	t_philo	*philo;
+
+	data = (t_data *)ptr;
+	philo = data->philo1;
+	while (true)
+	{
+		pthread_mutex_lock(philo->lock);
+		if (get_time() - philo->last_eaten>= data->time_to_die)
+		{
+			pthread_mutex_unlock(philo->lock);
+			pthread_mutex_lock(data->lock);
+			data->should_stop = true;
+			pthread_mutex_unlock(data->lock);
+			break;
+		}
+		pthread_mutex_unlock(philo->lock);
+		philo = philo->philo_r;
+	}
+	return (NULL);
+}
+			
+
+
