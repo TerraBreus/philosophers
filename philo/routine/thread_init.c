16f7ce6e62edaffd426731d9017c7e1cf2d2696e
TerraMint <studentbreus@gmail.com>
2025-09-12 11:13:20 +0200

    feat: init for ober

--- philo/routine/thread_init.c
+++ philo/routine/thread_init.c
@@ -4,12 +4,14 @@ int	thread_init(t_data *data)
 {
 	int	i;
 	t_philo	*philo_n;
+	pthread_t	ober_tid;
 
 	philo_n = data->philo1;
 	i = -1;
 	data->start_time = get_time();
+	pthread_create(&ober_tid, NULL, ober_routine, (void *) data);
 	while (++i < data->n_philo)
-	{
+{
 		pthread_create(&(philo_n->tid), NULL, philo_routine,(void *) philo_n);
 		philo_n = philo_n->philo_r;
 	}
