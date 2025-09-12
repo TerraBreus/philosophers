e9f48bf8862b36d3df66ccfdc742b314e0d4426c
TerraBreus <studentbreus@gmail.com>
2025-09-12 16:36:14 +0200

    feat: adding philo id to print

--- philo/debug/print_timelog.c
+++ philo/debug/print_timelog.c
@@ -3,6 +3,7 @@
 void	print_timelog(long current_time, t_philo *philo, t_data *data)
 {
 	printf("\n\nPRINTLOG\n");
+	printf("Philo: %d\n", philo->ID);
 	printf("Current time: %ld\n", current_time);
 	printf("last_eaten %ld\n", philo->last_eaten);
 	printf("time2die %d\n", data->time_to_die);
