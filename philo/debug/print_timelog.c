c7428948ba1c3694eb3ddc1ce4100568ad1b935b
TerraMint <studentbreus@gmail.com>
2025-09-12 11:24:31 +0200

    chore: refactor function for overview

--- philo/debug/print_timelog.c
+++ philo/debug/print_timelog.c
@@ -2,5 +2,10 @@
 
 void	print_timelog(long current_time, t_philo *philo, t_data *data)
 {
-	printf("Current time: %ld\nlast_eaten %ld\ntime2die %d\n", current_time, philo->last_eaten, data->time_to_die);
+	printf("\n\nPRINTLOG\n");
+	printf("Current time: %ld\n", current_time);
+	printf("last_eaten %ld\n", philo->last_eaten);
+	printf("time2die %d\n", data->time_to_die);
+	printf("evaluated time%ld\n", current_time - philo->last_eaten);
+	printf("\n\n\n");
 }
