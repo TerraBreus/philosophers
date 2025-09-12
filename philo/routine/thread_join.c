571e9272b59e4712a1c6d7327605552b1a00cfe2
TerraBreus <studentbreus@gmail.com>
2025-09-12 16:36:49 +0200

    feat: extra function to see where program keeps waiting

--- philo/routine/thread_join.c
+++ philo/routine/thread_join.c
@@ -1,4 +1,8 @@
 #include "philo.h"
+void	update_user(t_philo *philo)
+{
+	printf("Philosopher %d has ended\n", philo->ID);
+}
 
 int	thread_join(t_data *data)
 {
@@ -11,6 +15,7 @@ int	thread_join(t_data *data)
 	{
 		if (pthread_join((philo_n->tid), NULL) != 0)
 			return (-1);
+		update_user(philo_n);
 		philo_n = philo_n->philo_r;
 	}
 	return (0);
