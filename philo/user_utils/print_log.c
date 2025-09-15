6dfec636c2a74a01d4fab32803a5b2124d27b593
TerraBreus <studentbreus@gmail.com>
2025-09-15 10:38:29 +0200

    feat: checking whether philo have died before printing message

--- philo/user_utils/print_log.c
+++ philo/user_utils/print_log.c
@@ -5,7 +5,10 @@ void	print_log(long timestamp,t_philo *philo, char *action)
 	long	time;
 
 	time = timestamp - philo->data->start_time;
-	printf("%lu %i %s", time, philo->ID, action);
+	pthread_mutex_lock(philo->data->lock);
+	if (philo->data->should_stop == false)
+		printf("%lu %i %s", time, philo->ID, action);
+	pthread_mutex_unlock(philo->data->lock);
 }
 
 //I don't know what else to put here
