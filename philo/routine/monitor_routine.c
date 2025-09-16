dbb9356c5319098d21c1d71dcdaed15ef355dffc
TerraBreus <studentbreus@gmail.com>
2025-09-16 16:28:30 +0200

    fix: monitor also ends routine when should_stop is true

--- philo/routine/monitor_routine.c
+++ philo/routine/monitor_routine.c
@@ -9,6 +9,13 @@ void	*monitor_routine(void *ptr)
 	philo = data->philo1;
 	while (true)
 	{
+		pthread_mutex_lock(data->lock);
+		if (data->should_stop == true)
+		{
+			pthread_mutex_unlock(data->lock);
+			break;
+		}
+		pthread_mutex_unlock(data->lock);
 		pthread_mutex_lock(philo->lock);
 		if (philo->eat_count >= data->total_eat_limit)
 		{
