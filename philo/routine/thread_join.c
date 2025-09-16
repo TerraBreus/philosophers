5725dbef0a7d973b852b568791ff3689d247a690
TerraBreus <studentbreus@gmail.com>
2025-09-16 16:13:07 +0200

    fix: joining ober and monitor threads at end

--- philo/routine/thread_join.c
+++ philo/routine/thread_join.c
@@ -17,5 +17,12 @@ int	thread_join(t_data *data)
 			return (-1);
 		philo_n = philo_n->philo_r;
 	}
+	if (pthread_join(*(data->ober_tid), NULL) != 0)
+		return (-1);
+	if (data->monitor_tid != NULL)
+	{
+		if (pthread_join(*(data->monitor_tid), NULL) != 0)
+			return (-1);
+	}
 	return (0);
 }
