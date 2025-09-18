90bc87c70b8c3c58edfb4341adc3cc6b67045511
TerraBreus <studentbreus@gmail.com>
2025-09-18 15:42:39 +0200

    fix: set last_eaten for philo to start of simulation

--- philo/routine/single_philo.c
+++ philo/routine/single_philo.c
@@ -7,6 +7,9 @@ void	*single_philo(void *data_ptr)
 
 	data = (t_data *) data_ptr;
 	philo = data->philo1;
+	pthread_mutex_lock(philo->lock);
+	philo->last_eaten = data->start_time;
+	pthread_mutex_unlock(philo->lock);
 	pthread_mutex_lock(philo->fork_r);
 	print_log(get_time(), philo, FORK);
 	ft_usleep(data->time_to_die);
