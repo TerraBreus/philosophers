0134f7159a3eb2e3ac88e4c6119b53c8affe91e4
TerraBreus <studentbreus@gmail.com>
2025-09-27 15:34:08 +0200

    feat: added routine for uneven philosophers

--- 2_philo/simulation.c
+++ 2_philo/simulation.c
@@ -33,6 +33,7 @@ void	*philo_even(void *ptr)
 
 		philo = (t_philo *)ptr;
 		data = philo->data;
+		atomic_store(&philo->last_eaten, data->start_time);
 		while (true)
 		{
 				philo_even_eat(philo, data);
@@ -46,6 +47,49 @@ void	*philo_even(void *ptr)
 				ft_usleep(data->time_to_think);
 				if (atomic_load(&data->should_stop) == true)
 						break ;
+		}
+		return (NULL);
+}
+
+void	philo_uneven_eat(t_philo *philo, t_data *data)
+{
+		pthread_mutex_lock(philo->fork_l);
+		print_log(philo, data->log_mutex, FORK);
+		pthread_mutex_lock(philo->fork_r);
+		atomic_store(&philo->last_eaten, get_time());
+		if (atomic_fetch_add(&philo->eat_count, 1) + 1 == data->total_eat_limit)
+				atomic_store(&philo->eat_limit_reached, true);
+		print_log(philo, data->log_mutex, EAT);
+		ft_usleep(data->time_to_sleep);
+		pthread_mutex_unlock(philo->fork_r);
+		pthread_mutex_unlock(philo->fork_l);
+}
+
+void	*philo_uneven(void *ptr)
+{
+		t_philo	*philo;
+		t_data	*data;
+
+		philo = (t_philo *)ptr;
+		data = philo->data;
+		atomic_store(&philo->last_eaten, data->start_time);
+		if (data->time_to_think > 3)
+				ft_usleep(data->time_to_think / 2);
+		else
+				ft_usleep(1);
+		while (true)
+		{
+				philo_uneven_eat(philo, data);
+				if (atomic_load(&data->should_stop) == true)
+						break ;
+				print_log(philo, data->log_mutex, SLEEP);
+				ft_usleep(data->time_to_sleep);
+				if (atomic_load(&data->should_stop) == true)
+						break ;
+				print_log(philo, data->log_mutex, THINK);
+				ft_usleep(data->time_to_think);
+				if (atomic_load(&data->should_stop) == true)
+						break;
 		}
 		return (NULL);
 }
