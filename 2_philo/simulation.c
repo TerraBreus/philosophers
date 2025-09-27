de8ffcf82c200522d6e59326d354b83ec80bbbc4
TerraBreus <studentbreus@gmail.com>
2025-09-27 15:24:32 +0200

    feat: added eating routine for even philosopher

--- 2_philo/simulation.c
+++ 2_philo/simulation.c
@@ -12,6 +12,20 @@
 
 #include "philo.h"
 
+void	philo_even_eat(t_philo *philo, t_data *data)
+{
+		pthread_mutex_lock(philo->fork_r);
+		print_log(philo, data->log_mutex, FORK);
+		pthread_mutex_lock(philo->fork_l);
+		atomic_store(&philo->last_eaten, get_time());
+		if (atomic_fetch_add(&philo->eat_count, 1) + 1 == data->total_eat_limit)
+				atomic_store(&philo->eat_limit_reached, true);
+		print_log(philo, data->log_mutex, EAT);
+		ft_usleep(data->time_to_sleep);
+		pthread_mutex_unlock(philo->fork_r);
+		pthread_mutex_unlock(philo->fork_l);
+}
+
 void	*philo_even(void *ptr)
 {
 		t_philo	*philo;
@@ -21,31 +35,19 @@ void	*philo_even(void *ptr)
 		data = philo->data;
 		while (true)
 		{
-				//EAT
-				pthread_mutex_lock(philo->fork_r);
-				print_log(philo, data->log_mutex, FORK);
-				pthread_mutex_lock(philo->fork_l);
-				atomic_store(&philo->last_eaten, get_time());
-				atomic_fetch_add(&philo->eat_count, 1);
-				if (atomic_load(&philo->eat_count) == data->total_eat_limit)
-						atomic_store(&philo->eat_limit_reached, true);
-				print_log(philo, data->log_mutex, EAT);
-				ft_usleep(data->time_to_sleep);
-				pthread_mutex_unlock(philo->fork_r);
-				pthread_mutex_unlock(philo->fork_l);
+				philo_even_eat(philo, data);
 				if (atomic_load(&data->should_stop) == true)
 						break ;
-				//SLEEP
 				print_log(philo, data->log_mutex, SLEEP);
 				ft_usleep(data->time_to_sleep);
 				if (atomic_load(&data->should_stop) == true)
 						break ;
-				//THINK
 				print_log(philo, data->log_mutex, THINK);
-				ft_usleep(time_to_think);
+				ft_usleep(data->time_to_think);
 				if (atomic_load(&data->should_stop) == true)
 						break ;
 		}
+		return (NULL);
 }
 
 void	start_simulation(t_data *data)
