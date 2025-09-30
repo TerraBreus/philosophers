4340f13fed3c9ba32998846f823075a0633193d9
TerraMint <studentbreus@gmail.com>
2025-09-30 15:44:32 +0200

    feat: adding simple print function for data

--- 2_philo/main.c
+++ 2_philo/main.c
@@ -12,12 +12,20 @@
 
 #include "philo.h"
 
+static void	print_data(t_data *data)
+{
+	printf("time2die%d\n", data->time_to_die);
+	printf("time2eatp%d\n", data->time_to_eat);
+	printf("time2sleep%d\n", data->time_to_sleep);
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	*data;
 
 	check_input(argc, argv);
 	data = init_structs(argc, argv);
+	print_data(data);
 	start_simulation(data);
 	end_simulation(data);
 	cleanup_program(data, data->philo1, data->n_philo);
