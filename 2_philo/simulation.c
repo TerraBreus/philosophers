b77c512040166efdbcc26508d2fef49e4f366397
TerraBreus <studentbreus@gmail.com>
2025-10-04 15:18:18 +0200

    refactor: removing routines

--- 2_philo/simulation.c
+++ 2_philo/simulation.c
@@ -6,97 +6,11 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 13:33:50 by zivanov           #+#    #+#             */
-/*   Updated: 2025/09/27 13:33:58 by zivanov          ###   ########.fr       */
+/*   Updated: 2025/10/04 15:01:01 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	philo_even_eat(t_philo *philo, t_data *data)
-{
-	pthread_mutex_lock(philo->fork_r);
-	print_log(philo, data->log_mutex, FORK);
-	pthread_mutex_lock(philo->fork_l);
-	atomic_store(&philo->last_eaten, get_time());
-	if (atomic_fetch_add(&philo->eat_count, 1) + 1 == data->total_eat_limit)
-		atomic_store(&philo->eat_limit_reached, true);
-	print_log(philo, data->log_mutex, EAT);
-	ft_usleep(data->time_to_eat);
-	pthread_mutex_unlock(philo->fork_r);
-	pthread_mutex_unlock(philo->fork_l);
-}
-
-void	*philo_even(void *ptr)
-{
-	t_philo	*philo;
-	t_data	*data;
-
-	philo = (t_philo *)ptr;
-	data = philo->data;
-	atomic_fetch_add(&data->n_ready, 1);
-	while (!atomic_load(&data->simulation_ready))
-	atomic_store(&philo->last_eaten, data->start_time);
-	while (true)
-	{
-		philo_even_eat(philo, data);
-		if (atomic_load(&data->should_stop) == true)
-			break ;
-		print_log(philo, data->log_mutex, SLEEP);
-		ft_usleep(data->time_to_sleep);
-		if (atomic_load(&data->should_stop) == true)
-			break ;
-		print_log(philo, data->log_mutex, THINK);
-		ft_usleep(data->time_to_think);
-		if (atomic_load(&data->should_stop) == true)
-			break ;
-	}
-	return (NULL);
-}
-
-void	philo_uneven_eat(t_philo *philo, t_data *data)
-{
-	pthread_mutex_lock(philo->fork_l);
-	print_log(philo, data->log_mutex, FORK);
-	pthread_mutex_lock(philo->fork_r);
-	atomic_store(&philo->last_eaten, get_time());
-	if (atomic_fetch_add(&philo->eat_count, 1) + 1 == data->total_eat_limit)
-		atomic_store(&philo->eat_limit_reached, true);
-	print_log(philo, data->log_mutex, EAT);
-	ft_usleep(data->time_to_eat);
-	pthread_mutex_unlock(philo->fork_r);
-	pthread_mutex_unlock(philo->fork_l);
-}
-
-void	*philo_uneven(void *ptr)
-{
-	t_philo	*philo;
-	t_data	*data;
-
-	philo = (t_philo *)ptr;
-	data = philo->data;
-	atomic_fetch_add(&data->n_ready, 1);
-	while (!atomic_load(&data->simulation_ready))
-	atomic_store(&philo->last_eaten, data->start_time);
-	if (data->time_to_think > 3)
-		ft_usleep(data->time_to_think / 2);
-	else
-		ft_usleep(4);
-	while (true)
-	{
-		philo_uneven_eat(philo, data);
-		if (atomic_load(&data->should_stop) == true)
-			break ;
-		print_log(philo, data->log_mutex, SLEEP);
-		ft_usleep(data->time_to_sleep);
-		if (atomic_load(&data->should_stop) == true)
-			break ;
-		print_log(philo, data->log_mutex, THINK);
-		ft_usleep(data->time_to_think);
-		if (atomic_load(&data->should_stop) == true)
-			break ;
-	}
-	return (NULL);
-}
 
 void	end_simulation(t_data *data)
 {
@@ -129,6 +43,7 @@ void	start_simulation(t_data *data)
 	{
 		if (pthread_create(&philo->tid, NULL, single_philo, (void *)data) != 0)
 			return ;
+		data->threads_created++;
 	}
 	else
 	{
